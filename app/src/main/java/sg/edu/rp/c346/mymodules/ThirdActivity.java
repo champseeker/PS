1ac8ae2e24fbfa7c71ccd6d784cda45e196eda35
18016165 <18016165@myrp.edu.sg>
2019-05-17 22:27:58 +0800

    Submission 2.0

--- app/src/main/java/sg/edu/rp/c346/mymodules/ThirdActivity.java
+++ app/src/main/java/sg/edu/rp/c346/mymodules/ThirdActivity.java
@@ -17,12 +17,14 @@ public class ThirdActivity extends AppCompatActivity {
         tvAnswer2 = findViewById(R.id.textView2);
 
         Intent intentReceived = getIntent();
+
         String value1 = intentReceived.getStringExtra("value1");
         String value2 = intentReceived.getStringExtra("value2");
         String value3 = intentReceived.getStringExtra("value3");
         String value4 = intentReceived.getStringExtra("value4");
         String value5 = intentReceived.getStringExtra("value5");
         String value6 = intentReceived.getStringExtra("value6");
+
         tvAnswer2.setText("Module Code: " + value1 + "\nModule Name: " + value2 + "\nAcademic Year: " + value3 + "\nSemester: "+ value4 + "\nModule Credit: " + value5 + "\nVenue: " + value6);
 
     }
